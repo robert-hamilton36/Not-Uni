a6db4083ea15fe2b9444053d0f5b5f84f686ae5c
student <student@eda-bc-07.localdomain>
2020-12-03 09:35:36 +1300

    add firebase.js, beginnings of auth

---
+++ client/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+let firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+};
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+
+export const auth = app.auth()
+export default app
