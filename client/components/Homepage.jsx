cb2de81cc35194c091be28209afc64cdc6c2b0a8
olivia-gambitsis <o.gambitsis@gmail.com>
2020-12-03 13:25:54 +1300

    get modules into global state

--- client/components/Homepage.jsx
+++ client/components/Homepage.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
-
+import {connect} from 'react-redux'
+import {fetchModules} from '../actions/index'
 
 class HomePage extends React.Component{
+
+  state={
+    searchValue: ''
+  }
+
+  componentDidMount (){
+    this.props.dispatch(fetchModules())
+  }
 
 
   
@@ -20,3 +29,11 @@ class HomePage extends React.Component{
   }
 
 }
+
+
+function mapStateToProps(globalState) {
+  return {
+    modules: globalState.modules
+  }
+}
+export default connect(mapStateToProps)(Home)
