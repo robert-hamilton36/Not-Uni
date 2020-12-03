a7a197d02e930071b372c39e82740aff1f8c2229
robert-hamilton36 <69886955+robert-hamilton36@users.noreply.github.com>
2020-12-03 22:36:21 +1300

    quick dirty attempt at logout(success?) and log in(not so much)

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { auth } from '../firebase'
+import { setUser } from '../actions'
+import { connect } from 'react-redux'
 
 
 
-class Nav extends React.Component {
+
+
+
+export class Nav extends React.Component {
+
+  handleClick= () =>{
+    auth.signOut()
+    .then(this.props.dispatch(setUser("anon")))
+  }
+
   render() {
     return (
       <>
@@ -34,10 +46,11 @@ class Nav extends React.Component {
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>
             </div>
           </Link>
+          <button onClick={this.handleClick}>Sign Out</button>
         </nav>
       </>
     )
   }
 }
 
-export default Nav
+export default connect()(Nav)
--- client/components/Register.jsx
+++ client/components/Register.jsx
@@ -62,9 +62,6 @@ export class Register extends React.Component {
   
   }
 
-  componentWillUnmount(){
-    this.unsubscribe()
-  }
 
 
   render(){
