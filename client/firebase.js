e64fa2789bba5518adb60a9c3dc739989ca05263
student <student@eda-bc-06.localdomain>
2020-12-06 15:38:17 +1300

    made firebase config from .env

--- client/firebase.js
+++ client/firebase.js
@@ -1,15 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 
-// let firebaseConfig = {
-//   apiKey: process.env.API_KEY,
-//   authDomain: process.env.authDomain,
-//   databaseURL: process.env.databaseURL,
-//   projectId: process.env.projectId,
-//   storageBucket: process.env.storageBucket,
-//   messagingSenderId: process.env.messagingSenderId,
-//   appId: process.env.appId
-// }
+let firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId
+}
 
 
 
