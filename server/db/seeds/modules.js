1ef99f8fdf101af0dc64fd5ec05cda2df98680d1
corian-youdale <70251240+corian-youdale@users.noreply.github.com>
2020-12-07 16:14:14 +1300

    updating c# seeds

--- server/db/seeds/modules.js
+++ server/db/seeds/modules.js
@@ -6,7 +6,7 @@ exports.seed = function (knex) {
       return knex('modules').insert([
         { id: 101, title: 'Intro To JavaScript', user_id: 10001, category: 'JavaScript', duration: 15, description: 'The Perfect Place to Start Learning About JavaScript', number_of_elements: 5 },
         { id: 102, title: 'Intro To C++', user_id: 10002, category: 'C++', duration: 15, description: 'Learn the Basics of C++', number_of_elements: 5 },
-        { id: 103, title: 'The Language of C', user_id: 10003, category: 'C', duration: 15, description: 'C How Easy It Is To Learn C!', number_of_elements: 5 },
+        { id: 103, title: 'The Language of C', user_id: 10003, category: 'Csharp', duration: 15, description: "C# (CSharp) - Because C Wasn't Easy Enough", number_of_elements: 5 },
         { id: 104, title: 'Intro To Ruby', user_id: 10004, category: 'Ruby', duration: 15, description: 'Ruby is a general-purpose language that is still popular and in high demand in the marketplace, as it’s more commonly used in Rails applications. Concise and readable, it is easy to pick up but also plenty powerful. Companies like Twitter, Soundcloud, Goodreads, and Kickstarter got their products off the ground with Ruby.', number_of_elements: 5 },
         { id: 105, title: 'Intro To Python', user_id: 10005, category: 'Python', duration: 15, description: 'Python - more than just a snake', number_of_elements: 5 },
         { id: 106, title: 'JavaScript with React', user_id: 10001, category: 'JavaScript', duration: 15, description: 'Start Using React Now!', number_of_elements: 5 },
