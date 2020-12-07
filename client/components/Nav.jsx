e17c2c2fc82d6f3c2e3a8b20818eddc087dcc86c
student <student@eda-bc-06.localdomain>
2020-12-07 16:24:28 +1300

    render nav bar profile and sign out based on isAuth

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -43,14 +43,15 @@ class Nav extends React.Component {
             </>}
 
           {/* if user is signed in */}
-
+          { this.props.isAuthenticated === true && <>
           <Link to="/profile"> 
             <div className ='text' > 
               <span> Account </span>
               <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/></svg>
             </div>
           </Link>
-          { this.props.isAuthenticated === true && <Link onClick={this.handleClick} to='/'><div className ='text'><span>Sign Out</span></div></Link>}
+          <Link onClick={this.handleClick} to='/'><div className ='text'><span>Sign Out</span></div></Link>
+          </>}
          </div>
         </nav>
       </>
