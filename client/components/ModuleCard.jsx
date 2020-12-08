7ade77614ddf636d831a1e20b88b5d5c391f6c62
student <student@eda-bc-06.localdomain>
2020-12-09 12:22:13 +1300

    moved delete into card

--- client/components/ModuleCard.jsx
+++ client/components/ModuleCard.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import Likes from './Likes'
 
 class ModuleCard extends React.Component {
+state={delete:this.props.delete}
+
   render () {
    
       let moduleDifficulty = ''
@@ -55,7 +57,7 @@ class ModuleCard extends React.Component {
         cardColor = ''
     }
 
-   
+   console.log(this.props)
     return (
       <>
 
@@ -63,6 +65,9 @@ class ModuleCard extends React.Component {
 
           <div className={'s-c-heading ' + cardColor}>
             <h1>{this.props.module.title}</h1>
+            {this.state.delete &&
+            <svg onClick={() => this.props.handleDelete(true, this.props.module)}xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M0 0h24v24H0V0z" fill="none"/><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z"/></svg>
+  }
             {/* {this.props.hasLoaded.authHasLoaded && <> {this.props.isAuthenticated && <Likes module={this.props.module} />}</>} */}
           </div>
           <div className='s-c-info'>
